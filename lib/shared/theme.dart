1b0b4033cddd5f8d474614d36b1983173c439c85
WAHYU <wahyuwidito24@gmail.com>
2023-12-20 11:44:06 +0700

    skill tes

---
+++ lib/shared/theme.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+Color kBlueColor = Color(0xff0038FF);
+Color kWhiteColor = Color(0xffFFFFFF);
+
+TextStyle blueTextStyle = GoogleFonts.poppins(
+  color: kBlueColor,
+);
+
+TextStyle whiteTextStyle = GoogleFonts.poppins(
+  color: kWhiteColor,
+);
+
+FontWeight medium = FontWeight.w500;
